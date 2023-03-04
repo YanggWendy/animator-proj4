c4fad617d291b9426813a1ccdbee436c01daaf19
KaixingWU <61577667+KaixingWU@users.noreply.github.com>
2023-03-04 15:12:38 +0800

    Update mat.h
    solve the problem of slow simulation of particle system

--- mat.h
+++ mat.h
@@ -137,37 +137,37 @@ template <typename T>
 void Mat<T>::printData()
 {
 
-	for (int i = 0; i < M; i++)
-	{
-		for (int j = 0; j < N; j++)
-			cout << m_data[i * N + j] << '\t';
-		cout << endl;
-	}
-	cout << endl;
+// 	for (int i = 0; i < M; i++)
+// 	{
+// 		for (int j = 0; j < N; j++)
+// 			cout << m_data[i * N + j] << '\t';
+// 		cout << endl;
+// 	}
+// 	cout << endl;
 }
 
 template <typename T>
 void Mat<T>::printData(T* data, int m, int n)
 {
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-			cout << data[i * n + j] << '\t';
-		cout << endl;
-	}
-	cout << endl;
+// 	for (int i = 0; i < m; i++)
+// 	{
+// 		for (int j = 0; j < n; j++)
+// 			cout << data[i * n + j] << '\t';
+// 		cout << endl;
+// 	}
+// 	cout << endl;
 }
 
 template <typename T>
 void Mat<T>::printData(T* data, int dim)
 {
-	for (int i = 0; i < dim; i++)
-	{
-		for (int j = 0; j < dim; j++)
-			cout << data[i * dim + j] << '\t';
-		cout << endl;
-	}
-	cout << endl;
+// 	for (int i = 0; i < dim; i++)
+// 	{
+// 		for (int j = 0; j < dim; j++)
+// 			cout << data[i * dim + j] << '\t';
+// 		cout << endl;
+// 	}
+// 	cout << endl;
 }
 
 template <typename T>
